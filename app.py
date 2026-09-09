122cab4f35ce2bf0b311ed031bb16ac53fd3d73b
imranchoudhury461-sys <imranchoudhury461@gmail.com>
2026-09-09 17:27:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ except:
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
-GROQ_MODEL = "llama-3.3-70b-versatile"
+GROQ_MODEL = [
+    "openai/gpt-oss-120b",
+    "openai/gpt-oss-20b",
+]
 
 TRUSTED_DOMAINS = [
     "bbc.com", "bbc.co.uk", "reuters.com", "apnews.com",
